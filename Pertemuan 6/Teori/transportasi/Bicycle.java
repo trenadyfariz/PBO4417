677501334ee555e645da153e6e6255501d796e6e
trenadyfariz <79986839+trenadyfariz@users.noreply.github.com>
2021-04-11 21:53:15 +0700

    Add files via upload

---
+++ Pertemuan 6/Teori/transportasi/Bicycle.java
@@ -0,0 +1,10 @@
+package transportasi;
+
+public class Bicycle{
+    public void ringBell(){
+        System.out.println("Kring...Kring...");
+    }
+    public void takeBrake(){
+        System.out.println("Berhenti...");
+    }
+}
--- Pertemuan 6/Teori/transportasi/Mobil.java
+++ Pertemuan 6/Teori/transportasi/Mobil.java
@@ -1,1 +1,16 @@
+package transportasi;
 
+public class Mobil{
+    double speed = 0;
+    String color = "Orange";
+
+    public void goStraight(){
+        System.out.println("Maju...");
+    }
+    public void turnLeft(){
+        System.out.println("Belok Kiri...");
+    }
+    public void turnRight(){
+        System.out.println("Belok Kanan");
+    }
+}
