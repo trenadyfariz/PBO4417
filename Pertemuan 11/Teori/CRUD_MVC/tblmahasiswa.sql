bb092d03d39748908b6aa429b73fb05e6a4ae273
trenadyfariz <79986839+trenadyfariz@users.noreply.github.com>
2021-06-01 22:22:08 +0700

    Add files via upload

---
+++ Pertemuan 11/Teori/CRUD_MVC/tblmahasiswa.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 01, 2021 at 05:18 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblmahasiswa`
+--
+
+CREATE TABLE `tblmahasiswa` (
+  `id` int(11) DEFAULT NULL,
+  `nim` varchar(20) DEFAULT NULL,
+  `nama` varchar(30) DEFAULT NULL,
+  `jk` varchar(15) DEFAULT NULL,
+  `alamat` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
